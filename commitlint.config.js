e2bd2cf7684704aa140258343edee306641a278a
Benjamin Macher <macher.benjamin@icloud.com>
2022-01-03 08:16:35 +0100

    tooling(commitlint): set body length

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'body-max-line-length': [2, 'always', Infinity],
     'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'refactor', 'test', 'chore', 'wip', 'style', 'tooling', 'revert']],
   },
 };
