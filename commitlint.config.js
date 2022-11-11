b3a99e68ce131c8e9ab90664cd0f86c517ea11c5
Benjamin Macher <32685580+bmacher@users.noreply.github.com>
2022-11-11 08:18:05 +0100

    tooling(commitlint): ignore dependabot commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,4 +4,10 @@ module.exports = {
     'body-max-line-length': [2, 'always', Infinity],
     'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'refactor', 'test', 'chore', 'wip', 'style', 'tooling', 'revert']],
   },
+
+  defaultIgnores: false,
+  ignores: [
+    // Ignore dependabot commits with large bodies.
+    (commit) => /^chore\(deps(-dev)?\): bump/.test(commit),
+  ],
 };
